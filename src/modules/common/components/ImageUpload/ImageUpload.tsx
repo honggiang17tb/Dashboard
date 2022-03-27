c3229f367a5d368714d3d5c5ffe6a0e573352ccc
honggiang17tb <honggiang17tb@gmail.com>
2022-03-27 11:33:30 +0700

    Render Img Product Details

---
+++ src/modules/common/components/ImageUpload/ImageUpload.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect } from "react";
+import ImageUploading, { ImageListType } from "react-images-uploading";
+
+
+interface Props {
+    data?: any
+    setValueUpdate: any
+}
+
+export function ImageUpload({ data, setValueUpdate }: Props) {
+    const [images, setImages] = React.useState<ImageListType>(data);
+    const maxNumber = 100;
+
+    const handleChange = (
+        imageList: ImageListType,
+        addUpdateIndex: number[] | undefined
+    ) => {
+        // Data for submit
+
+        console.log('image',imageList);
+        console.log('i' , addUpdateIndex);
+        
+        setImages(imageList as never[]);
+    };
+
+
+    useEffect(() => {
+        setValueUpdate((prev: any) => {
+            return { ...prev, imagesOrder: images.map((item) => { return typeof (item.file) == 'object' ? item.file.name : item.file }) }
+        })
+
+    }, [images])
+
+    
+
+    return (
+        <div className="upload__image">
+            <ImageUploading
+                multiple
+                value={images}
+                onChange={handleChange}
+                maxNumber={maxNumber}
+            >
+                {({
+                    imageList,
+                    onImageUpload,
+                    onImageRemove,
+                    isDragging,
+                    dragProps
+                }) => (
+                    // Write your building UI
+                    <div className="upload__image-wrapper">
+                        {imageList.map((image, index) => (
+                            <div key={index} className="image-item">
+                                <img src={image.dataURL} alt="" width="124px" height='124px' />
+                                <div className="image-item__btn-wrapper">
+
+                                    <i className="fa fa-times-circle" onClick={() => {
+                                        console.log(image.id);
+                                        onImageRemove(index)
+                                    }}
+                                    ></i>
+                                </div>
+                            </div>
+                        ))}
+                        <div
+                            className="file-cta"
+                            onClick={onImageUpload}
+                            {...dragProps}
+                        >
+                            <i className="fa fa-camera icon-camera"></i>
+                        </div>
+                    </div>
+                )}
+            </ImageUploading>
+        </div>
+    );
+}
--- src/modules/common/components/UploadImg/UploadImg.tsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-import ImageUploading, { ImageListType } from "react-images-uploading";
-
